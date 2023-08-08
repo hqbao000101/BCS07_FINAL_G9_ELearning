c9aa19835e7184fa846692fff6a01ae5746ef63d
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-08 23:02:26 +0700

    feat: footer-p2

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { HomeFilled, MailFilled, PhoneFilled } from "@ant-design/icons";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -5,92 +6,166 @@ const Footer = () => {
   return (
     <footer className="bg-[#f0f8ff]">
       <div className="w-full p-4 py-6 mx-auto max-w-screen-2xl lg:py-8">
-        <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <NavLink to="https://flowbite.com/" className="flex items-center">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap">
-                <span className="text-[#41b294]">E</span>
-                Learning
+        <div className="grid grid-cols-1 gap-10 text-center lg:grid-cols-5 md:grid-cols-2 lg:gap-0 md:text-left">
+          <div className="col-span-1 mb-6 lg:col-span-2 md:mb-0">
+            <NavLink
+              to="/"
+              className="flex items-center justify-center mb-4 md:justify-start"
+            >
+              <span className="relative self-center text-2xl font-bold whitespace-nowrap group">
+                <span className="text-[#41b294] drop-shadow-[5px_-2px_3px_#54d2c0] text-5xl me-2 group-hover:scale-105 inline-block duration-300">
+                  E
+                </span>
+                <span className="drop-shadow-[5px_-2px_3px_#54d2c0] font-[900] group-hover:scale-105 inline-block duration-300">
+                  Learning
+                </span>
+                <img
+                  src="./favicon.png"
+                  alt="Logo"
+                  className="absolute h-10 duration-300 -right-4 -top-3 group-hover:-top-4 group-hover:-right-5"
+                />
               </span>
             </NavLink>
+            <ul className="font-medium text-gray-500">
+              <NavLink to="https://cybersoft.edu.vn/" target="_blank">
+                <li className="flex items-center mb-4 cursor-pointer group">
+                  <PhoneFilled className="p-3 rounded-full bg-[#41b294] text-white group-hover:-translate-y-1 duration-300 inline-block" />
+                  <span className="inline-block duration-300 ms-2 group-hover:-translate-y-1 group-hover:text-[#41b294]">
+                    096-105-1014
+                  </span>
+                </li>
+              </NavLink>
+              <NavLink to="https://cybersoft.edu.vn/" target="_blank">
+                <li className="flex items-center mb-4 cursor-pointer group">
+                  <MailFilled className="p-3 rounded-full bg-[#41b294] text-white group-hover:-translate-y-1 duration-300 inline-block" />
+                  <span className="inline-block duration-300 ms-2 group-hover:-translate-y-1 group-hover:text-[#41b294]">
+                    info@cybersoft.edu.vn
+                  </span>
+                </li>
+              </NavLink>
+              <NavLink to="https://cybersoft.edu.vn/" target="_blank">
+                <li className="flex items-center mb-4 cursor-pointer group">
+                  <HomeFilled className="p-3 rounded-full bg-[#41b294] text-white group-hover:-translate-y-1 duration-300 inline-block" />
+                  <span className="inline-block duration-300 ms-2 group-hover:-translate-y-1 group-hover:text-[#41b294]">
+                    Tầng 5, toà nhà Suri, 112 Cao Thắng, Quận 3, TPHCM
+                  </span>
+                </li>
+              </NavLink>
+            </ul>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Resources
-              </h2>
-              <ul className="font-medium text-gray-500">
-                <li className="mb-4">
-                  <NavLink
-                    to="https://flowbite.com/"
-                    className="hover:underline"
-                  >
-                    Flowbite
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="https://tailwindcss.com/"
-                    className="hover:underline"
-                  >
-                    Tailwind CSS
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Follow us
-              </h2>
-              <ul className="font-medium text-gray-500">
-                <li className="mb-4">
-                  <NavLink
-                    to="https://github.com/themesberg/flowbite"
-                    className="hover:underline "
-                  >
-                    Github
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="https://discord.gg/4eeurUVvTy"
-                    className="hover:underline"
-                  >
-                    Discord
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
-                Legal
-              </h2>
-              <ul className="font-medium text-gray-500">
-                <li className="mb-4">
-                  <NavLink to="/" className="hover:underline">
-                    Privacy Policy
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/" className="hover:underline">
-                    Terms &amp; Conditions
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
+          <div>
+            <h2 className="mb-6 text-lg font-bold text-black uppercase md:border-l-4 border-[#41b294] ps-2 py-2 md:py-0 border-l-0">
+              Liên kết
+            </h2>
+            <ul className="font-medium text-gray-500">
+              <li className="mb-4">
+                <NavLink
+                  to="/"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} Trang chủ
+                </NavLink>
+              </li>
+              <li className="mb-4">
+                <NavLink
+                  to="/intro"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} Thông tin
+                </NavLink>
+              </li>
+              <li className="mb-4">
+                <NavLink
+                  to="/blog"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} Blog
+                </NavLink>
+              </li>
+              <li className="mb-4">
+                <NavLink
+                  to="/event"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} Sự kiện
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-lg font-bold text-black uppercase md:border-l-4 border-[#41b294] ps-2 py-2 md:py-0 border-l-0">
+              Khóa học
+            </h2>
+            <ul className="font-medium text-gray-500">
+              <li className="mb-4">
+                <NavLink
+                  to="/category/FrontEnd"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} ReactJS
+                </NavLink>
+              </li>
+              <li className="mb-4">
+                <NavLink
+                  to="/category/BackEnd"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} NodeJS
+                </NavLink>
+              </li>
+              <li className="mb-4">
+                <NavLink
+                  to="/category/FullStack"
+                  className="inline-block duration-300 hover:-translate-y-1 hover:text-[#41b294]"
+                >
+                  {">"} Fullstack
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-lg font-bold text-black uppercase md:border-l-4 border-[#41b294] ps-2 py-2 md:py-0 border-l-0">
+              Đăng ký tư vấn
+            </h2>
+            <form className="flex flex-col items-center md:block">
+              <input
+                type="text"
+                placeholder="Họ và tên"
+                className="bg-orange-100 focus:border-orange-400 w-2/3 md:w-full duration-300 outline-none px-5 py-2 mb-3 border-2 border-[#41b294] rounded-md"
+              />
+              <input
+                type="email"
+                placeholder="Địa chỉ email"
+                className="bg-orange-100 focus:border-orange-400 w-2/3 md:w-full duration-300 outline-none px-5 py-2 mb-3 border-2 border-[#41b294] rounded-md"
+              />
+              <input
+                type="text"
+                placeholder="Số điện thoại"
+                className="bg-orange-100 focus:border-orange-400 w-2/3 md:w-full duration-300 outline-none px-5 py-2 mb-3 border-2 border-[#41b294] rounded-md"
+              />
+              <button
+                type="button"
+                className="w-2/3 py-2 text-white duration-300 bg-orange-300 rounded-md md:w-full hover:bg-orange-400"
+              >
+                Đăng ký
+              </button>
+            </form>
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center">
-            Cyber E-Learning © 2023{" "}
-            <NavLink to="https://flowbite.com/" className="hover:underline">
-              by BCS07 | G9
-            </NavLink>
-            . All Rights Reserved.
+          <span className="text-sm text-black sm:text-center">
+            <NavLink to="/" className="hover:underline">
+              Cyber E-Learning
+            </NavLink>{" "}
+            © 2023 by BCS07 | G9. All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-            <NavLink to="/" className="text-gray-500 hover:text-gray-900">
+            <NavLink
+              to="https://www.facebook.com/lophocviet/"
+              className="text-gray-500 duration-150 hover:text-blue-500"
+              target="_blank"
+            >
               <svg
                 className="w-4 h-4"
                 aria-hidden="true"
@@ -106,35 +181,11 @@ const Footer = () => {
               </svg>
               <span className="sr-only">Facebook page</span>
             </NavLink>
-            <NavLink to="/" className="text-gray-500 hover:text-gray-900">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 21 16"
-              >
-                <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
-              </svg>
-              <span className="sr-only">Discord community</span>
-            </NavLink>
-            <NavLink to="/" className="text-gray-500 hover:text-gray-900">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 17"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Twitter page</span>
-            </NavLink>
-            <NavLink to="/" className="text-gray-500 hover:text-gray-900">
+            <NavLink
+              to="https://github.com/hqbao000101/BCS07_FINAL_G9_ELearning.git"
+              className="text-gray-500 duration-150 hover:text-black"
+              target="_blank"
+            >
               <svg
                 className="w-4 h-4"
                 aria-hidden="true"
@@ -150,21 +201,25 @@ const Footer = () => {
               </svg>
               <span className="sr-only">GitHub account</span>
             </NavLink>
-            <NavLink to="/" className="text-gray-500 hover:text-gray-900">
+            <NavLink
+              to="https://www.youtube.com/channel/UCQDXgEguMerxvCh_2dcxmtQ"
+              className="text-gray-500 duration-150 hover:text-red-500"
+              target="_blank"
+            >
               <svg
                 className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
-                viewBox="0 0 20 20"
+                viewBox="0 0 30 30"
               >
                 <path
                   fillRule="evenodd"
-                  d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                  d="M 15 4 C 10.814 4 5.3808594 5.0488281 5.3808594 5.0488281 L 5.3671875 5.0644531 C 3.4606632 5.3693645 2 7.0076245 2 9 L 2 15 L 2 15.001953 L 2 21 L 2 21.001953 A 4 4 0 0 0 5.3769531 24.945312 L 5.3808594 24.951172 C 5.3808594 24.951172 10.814 26.001953 15 26.001953 C 19.186 26.001953 24.619141 24.951172 24.619141 24.951172 L 24.621094 24.949219 A 4 4 0 0 0 28 21.001953 L 28 21 L 28 15.001953 L 28 15 L 28 9 A 4 4 0 0 0 24.623047 5.0546875 L 24.619141 5.0488281 C 24.619141 5.0488281 19.186 4 15 4 z M 12 10.398438 L 20 15 L 12 19.601562 L 12 10.398438 z"
                   clipRule="evenodd"
                 />
               </svg>
-              <span className="sr-only">Dribbble account</span>
+              <span className="sr-only">Youtube account</span>
             </NavLink>
           </div>
         </div>
