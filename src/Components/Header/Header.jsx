217b353f40fd1a24d44b8e2c71ed44b5d5ef3239
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-09 10:20:14 +0700

    update: login-navbar-active

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -127,9 +127,6 @@ const Header = () => {
             <NavLink
               to="/login"
               className="text-white hover:bg-orange-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none bg-orange-400 duration-300 hover:shadow-md"
-              onClick={() => {
-                dispatch(setNavbarActive(false));
-              }}
             >
               Đăng nhập
             </NavLink>
