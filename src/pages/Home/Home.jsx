af68475f71ba73cf69ff46aa3e8067e2535bfac4
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-12 12:22:03 +0700

    feat: home-layout-and-imgs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import HomeBanner1 from "../../assets/imgs/home_banner_01.png";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <div className="flex items-center p-4 py-6 mx-auto max-w-screen-2xl">
+        <div className="w-1/2">Home Title</div>
+        <div className="w-1/2">
+          <img
+            src={HomeBanner1}
+            alt="Home Banner 01"
+            className="object-cover w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
