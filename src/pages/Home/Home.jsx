717fdb3101fffd10d1d8f9f19886498277eabcf6
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-15 03:25:39 +0700

    feat: home-count-up-number

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -138,6 +138,29 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="px-4 py-10 mx-auto max-w-screen-2xl 2xl:px-0">
+        <h3 className="text-2xl font-bold text-orange-400">
+          Khóa học tham khảo
+        </h3>
+        <div className="grid gap-20 mt-8 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </div>
+      </div>
+      <div className="px-4 py-10 mx-auto max-w-screen-2xl 2xl:px-0">
+        <h3 className="text-2xl font-bold text-orange-400">
+          Khóa học Front-end
+        </h3>
+        <div className="grid gap-20 mt-8 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </div>
+      </div>
+      <HomeCountUp />
     </div>
   );
 };
