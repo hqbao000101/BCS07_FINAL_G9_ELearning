6152872a6a594e769acc705f6dc50fa8b5df54ac
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-15 22:10:28 +0700

    feat: home-courses-APIs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -138,66 +138,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="px-4 py-10 mx-auto max-w-screen-2xl 2xl:px-0">
-        <h3 className="text-2xl font-bold text-orange-400">
-          Khóa Học Tham Khảo
-        </h3>
-        <div className="grid gap-20 mt-8 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4">
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-        </div>
-      </div>
-      <div className="px-4 py-10 mx-auto max-w-screen-2xl 2xl:px-0">
-        <h3 className="text-2xl font-bold text-orange-400">
-          Khóa Học Front-end
-        </h3>
-        <div className="grid gap-20 mt-8 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4">
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-        </div>
-      </div>
-      <HomeCountUp />
-      <HomeCarousel />
-      <div className="px-4 pt-0 pb-10 mx-auto sm:py-20 max-w-screen-2xl 2xl:px-0">
-        <h3 className="text-2xl font-bold text-orange-400">
-          Đánh Giá Từ Học Viên
-        </h3>
-        <div className="flex flex-col items-center justify-between mt-10 sm:mt-20 md:flex-row">
-          <div>
-            <div className="w-3/5 px-6 py-5 mx-auto mb-10 bg-orange-400 rounded-b-full shadow-2xl sm:mb-20 sm:py-10 sm:px-12 sm:w-2/5 md:w-2/3 lg:w-1/2 xl:w-1/3 rounded-e-full md:mb-0">
-              <img
-                src={UserComment}
-                alt="Top User"
-                className="object-cover w-full"
-              />
-            </div>
-          </div>
-          <div>
-            <div className="flex text-lg">
-              <i className="text-xl sm:text-4xl fa-solid fa-quote-left text-main me-5 animate-pulse"></i>
-              <div>
-                <p className="text-sm leading-8 sm:leading-10 sm:text-base">
-                  Chương trình giảng dạy được biên soạn dành riêng cho các bạn
-                  Lập trình từ trái ngành hoặc đã có kiến thức theo cường độ
-                  cao, luôn được tinh chỉnh và tối ưu hóa theo thời gian bởi các
-                  thành viên sáng lập và giảng viên dày kinh nghiệm. Thực sự rất
-                  hay và hấp dẫn!
-                </p>
-                <div className="mt-5">
-                  <h4 className="font-medium text-main md:text-xl">Mei Mei</h4>
-                  <p className="text-sm text-gray-400 md:text-base">
-                    Học viên xuất sắc
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
