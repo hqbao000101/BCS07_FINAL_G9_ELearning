51db9324eee0b15419164e1cb6de2bdf3cafb7d8
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-23 18:31:21 +0700

    update: improve-UI-UX

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react'
-import BlogContent from '../../Components/Blog/BlogContent';
+import React, { useEffect } from "react";
+import BlogContent from "../../Components/Blog/BlogContent";
+import { CaretRightOutlined } from "@ant-design/icons";
 
 const Blog = (props) => {
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div>
-        <BlogContent />
+      <div className="py-16 text-white bg-orange-400">
+        <div className="px-4 mx-auto max-w-screen-2xl">
+          <h1 className="mb-2 text-4xl font-bold uppercase sm:text-5xl">
+            Blog
+          </h1>
+          <p className="flex items-center text-black sm:text-lg">
+            <CaretRightOutlined className="me-1 animate-pulse" />
+            Thông tin công nghệ số!
+          </p>
+        </div>
+      </div>
+      <BlogContent />
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
