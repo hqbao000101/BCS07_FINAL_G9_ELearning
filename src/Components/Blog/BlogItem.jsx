4cd2a6db9c3cb018c5accf466d8777972ad622cf
Huỳnh Quang Bảo <hbao592000@gmail.com>
2023-08-23 17:21:48 +0700

    update: libraries-and-styles

--- src/Components/Blog/BlogItem.jsx
+++ src/Components/Blog/BlogItem.jsx
@@ -1,27 +1,27 @@
 import { Col } from 'antd'
 import React from 'react';
 import { FaRegThumbsUp, FaEye, FaRegComment } from "react-icons/fa";
-import styles from './BlogItem.scss'
+import './BlogItem.scss'
 
 const BlogItem = (props) => {
     return (
-        <Col className='lg:px-5 px-10 mb-16 lg:pb-10' xs={24} md={12}>
-            <div className='w-full h-48 lg:h-60 overflow-hidden'>
-                <img className='object-contain w-full block' src={props.dataBlog.hinhAnh} alt="..." />
+        <Col className='px-10 mb-16 lg:px-5 lg:pb-10' xs={24} md={12}>
+            <div className='w-full h-48 overflow-hidden lg:h-60'>
+                <img className='block object-contain w-full' src={props.dataBlog.hinhAnh} alt="..." />
             </div>
-            <h1 className='text-xl font-semibold py-3 h-20 overflow-hidden'>{props.dataBlog.tieuDe}</h1>
+            <h1 className='h-20 py-3 overflow-hidden text-xl font-semibold'>{props.dataBlog.tieuDe}</h1>
             <div className='block lg:flex justify-between items-center text-[16px]'>
                 <div className='flex'>
-                    <div className='mr-3 flex items-center'>
-                        <FaRegThumbsUp className='text-lg mr-1 text-teal-500 cursor-pointer'/>
+                    <div className='flex items-center mr-3'>
+                        <FaRegThumbsUp className='mr-1 text-lg text-teal-500 cursor-pointer'/>
                         <span>300</span>
                     </div>
-                    <div className='mr-3 flex items-center'>
-                        <FaRegComment className='text-lg mr-1 text-teal-500 cursor-pointer'/>
+                    <div className='flex items-center mr-3'>
+                        <FaRegComment className='mr-1 text-lg text-teal-500 cursor-pointer'/>
                         <span>500</span>
                     </div>
                     <div className='flex items-center'>
-                        <FaEye className='text-lg mr-1 text-teal-500 cursor-pointer'/>
+                        <FaEye className='mr-1 text-lg text-teal-500 cursor-pointer'/>
                         <span>2500</span>
                     </div>
                 </div>
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect } from 'react'
 import BlogContent from '../../Components/Blog/BlogContent';
-import Banner from '../../Components/Banner/Banner';
-import Footer from '../../Components/Footer/Footer';
 
 const Blog = (props) => {
-
     useEffect(() => {
       window.scrollTo(0, 0);
     }, [])
