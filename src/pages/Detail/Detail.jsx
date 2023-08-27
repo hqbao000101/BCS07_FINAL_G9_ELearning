1498b3c539636c0f3b376aa90858adcabdd47ff0
Dang_Thuy_Linh <Lh2982001@gmail.com>
2023-08-27 17:19:02 +0700

    +details

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import "./Details.scss";
+import ReactBanner from "../../assets/imgs/card_react_sample.jpg";
+import UserComment from "../../assets/imgs/user_feedback.png";
 
-const Detail = () => {
-  return <div>Detail</div>;
+const Details = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-between mt-10 sm:mt-20 md:flex-row">
+          <div class="margin">
+            <div class="img-margin">
+              <img
+                src={ReactBanner}
+                alt="Course Details"
+                className="object-cover w-full"
+              />
+            </div>
+          </div>
+          <div class="text-margin">
+            <div className="flex text-lg">
+              <i className="text-xl sm:text-4xl fa-solid text-main me-5 animate-pulse"></i>
+              <div>
+                <p className="text-sm leading-8 sm:leading-10 sm:text-base">
+                Khóa học này dạy cho bạn cách lập trình hiện đại với các hướng dẫn tùy chỉnh cho cả người mới học và người lập trình có kinh nghiệm. Nội dung sẽ bao gồm React, Golang, Python và C. Học sinh sẽ có cơ hội tiếp cận với những người cố vấn, những người sẽ giúp họ vượt trội trong sự nghiệp.
+                </p>
+                <div className="mt-5">
+                  <h4 className="font-medium text-main md:text-xl"> <button>MUA KHÓA HỌC</button> </h4>
+                  <p className="text-sm text-gray-400 md:text-base">
+                  120.000đ
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-between mt-10 sm:mt-20 md:flex-row">
+          <div>
+            <div className="w-3/5 px-6 py-5 mx-auto mb-10 bg-orange-400 rounded-b-full shadow-2xl sm:mb-20 sm:py-10 sm:px-12 sm:w-2/5 md:w-2/3 lg:w-1/2 xl:w-1/3 rounded-e-full md:mb-0">
+              <img
+                src={UserComment}
+                alt="Top User"
+                className="object-cover w-full"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex text-lg">
+              <i className="text-xl sm:text-4xl fa-solid fa-quote-left text-main me-5 animate-pulse"></i>
+              <div>
+                <p className="text-sm leading-8 sm:leading-10 sm:text-base">
+                  Chương trình giảng dạy được biên soạn dành riêng cho các bạn
+                  Lập trình từ trái ngành hoặc đã có kiến thức theo cường độ
+                  cao, luôn được tinh chỉnh và tối ưu hóa theo thời gian bởi các
+                  thành viên sáng lập và giảng viên dày kinh nghiệm. Thực sự rất
+                  hay và hấp dẫn!
+                </p>
+                <div className="mt-5">
+                  <h4 className="font-medium text-main md:text-xl">Mei Mei</h4>
+                  <p className="text-sm text-gray-400 md:text-base">
+                    Học viên xuất sắc
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 };
 
-export default Detail;
+export default Details;
